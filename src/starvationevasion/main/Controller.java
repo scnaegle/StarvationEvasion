5c7d242ca2b9d4a9bf3e1d1c627508e045caf83b
Ally <agoodmanjanow@unm.edu>
2015-11-09 21:46:58 -0700

    Added code for pushing buttons

--- src/starvationevasion/main/Controller.java
+++ src/starvationevasion/main/Controller.java
@@ -1,4 +1,34 @@
 package starvationevasion.main;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class Controller{
+
+  @FXML
+  private Button appleButton;
+  @FXML
+  private Button grainsButton;
+
+  @FXML
+  public void buttonPressed(ActionEvent event)
+  {
+    Button button = (Button)event.getSource();
+    if(button == appleButton){
+      System.out.println("Pressed apple button");
+    }
+    else if(button == grainsButton){
+      System.out.println("Pressed grains button");
+    }
+
+
+  }
+
+
+
+
 }
+
