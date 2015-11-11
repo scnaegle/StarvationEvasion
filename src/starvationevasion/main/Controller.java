341a12e42c139ca26d1c7ca0567bd7ad11fea037
Ally <agoodmanjanow@unm.edu>
2015-11-11 15:56:55 -0700

    Adding to gui

--- src/starvationevasion/main/Controller.java
+++ src/starvationevasion/main/Controller.java
@@ -9,21 +9,33 @@ import javafx.scene.image.ImageView;
 public class Controller{
 
   @FXML
+  private Button drawCardButton;
+  @FXML
   private Button appleButton;
   @FXML
   private Button grainsButton;
+
 
   @FXML
   public void buttonPressed(ActionEvent event)
   {
     Button button = (Button)event.getSource();
-    if(button == appleButton){
+    if(button == drawCardButton){
+      System.out.println("Drawing card from deck");
+    }
+    else if(button == appleButton){
       System.out.println("Pressed apple button");
     }
     else if(button == grainsButton){
       System.out.println("Pressed grains button");
     }
 
+
+  }
+
+  @FXML
+  public void updateLabel()
+  {
 
   }
 
