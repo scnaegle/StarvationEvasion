e681d21226e868ccc53a8b8ce599f48532c5512e
Sean Naegle <sean@naegles.com>
2015-11-14 12:18:06 -0700

    started filling our decks

--- src/starvationevasion/teamrocket/Deck.java
+++ src/starvationevasion/teamrocket/Deck.java
@@ -1,7 +1,34 @@
 package starvationevasion.teamrocket;
 
+import starvationevasion.common.EnumPolicy;
+import starvationevasion.common.EnumRegion;
+import starvationevasion.common.PolicyCard;
+
+import java.util.ArrayList;
+
 /**
  * Created by scnaegl on 11/14/15.
  */
 public class Deck {
+  ArrayList<PolicyCard> cards = new ArrayList<>();
+  ArrayList<PolicyCard> discard = new ArrayList<>();
+
+  private EnumRegion region;
+
+  public Deck(EnumRegion region) {
+    this.region = region;
+    this.cards = createDeck(region);
+    this.discard = new ArrayList<>();
+  }
+
+  public PolicyCard drawCard() {
+    PolicyCard card = cards.get(0);
+    return card;
+  }
+
+  private ArrayList<PolicyCard> createDeck(EnumRegion region) {
+    ArrayList<PolicyCard> cards = new ArrayList<>();
+    // TODO pull cards from XML file
+    return cards;
+  }
 }
