7f488ffa35ac7cb2bb648115706cd112aec5f86d
Ally <star.goodman@gmail.com>
2015-11-14 17:00:14 -0700

    Fixed clicking on map so only one region is selected at a time.

--- src/starvationevasion/teamrocket/gui/GuiController.java
+++ src/starvationevasion/teamrocket/gui/GuiController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import starvationevasion.common.EnumRegion;
 
 
 public class GuiController
@@ -61,8 +62,9 @@ public class GuiController
   private boolean southeastSelected;
   private boolean sPlainSelected;
 
-  private boolean[] regionSelected = {caliSelected,heartlandSelected,mountainSelected,nPlainSelected,northeastSelected
-  , southeastSelected, sPlainSelected};
+  private boolean[] regionSelected =
+      {caliSelected, heartlandSelected, mountainSelected, nPlainSelected, northeastSelected
+          , southeastSelected, sPlainSelected};
 
   @FXML
   public void buttonPressed(ActionEvent event)
@@ -91,178 +93,185 @@ public class GuiController
   }
 
 
-//  @FXML
-//  public void mouseEntered(MouseEvent event)
-//  {
-//    System.out.println("reached here");
-//    ImageView image = (ImageView)event.getSource();
-//
-//
-//  }
-
   @FXML
   public void mouseMoved(MouseEvent event)
   {
     System.out.println("MOVING");
     double x = event.getX();
     double y = event.getY();
-//    System.out.println(x + "," + y);
-    if(!clickedRegion)
-    {
-      caliLabel.setVisible(false);
-      mountLabel.setVisible(false);
-      nPlainLabel.setVisible(false);
-      sPlainLabel.setVisible(false);
-      heartLabel.setVisible(false);
-      seLabel.setVisible(false);
-      neLabel.setVisible(false);
 
-      if (ImageRegion.CALIFORNIA.contains(x, y))//(x>25 && x <80 && y>70 && y<150)
-      {
-        caliLabel.setVisible(true);
-      }
-      if (ImageRegion.MOUNTAINST.contains(x, y))
-      {
-        mountLabel.setVisible(true);
-      }
-      if (ImageRegion.NORTHPLAINS.contains(x, y))// || x>200 && x<250 && y>=5 && y<=160)
-      {
-        nPlainLabel.setVisible(true);
-      }
-      else if (ImageRegion.SOUTHPLAINS.contains(x, y))
-      {
-        sPlainLabel.setVisible(true);
-      }
-      else if (ImageRegion.HEARTLAND.contains(x, y))
-      {
-        heartLabel.setVisible(true);
-      }
-      else if (ImageRegion.NORTHEAST.contains(x, y))
-      {
-        neLabel.setVisible(true);
-      }
-      else if (ImageRegion.SOUTHEAST.contains(x, y))
-      {
-        seLabel.setVisible(true);
-      }
+    hideLabels();
+
+    if (ImageRegion.CALIFORNIA.contains(x, y))//(x>25 && x <80 && y>70 && y<150)
+    {
+      caliLabel.setVisible(true);
     }
+    if (ImageRegion.MOUNTAINST.contains(x, y))
+    {
+      mountLabel.setVisible(true);
+    }
+    if (ImageRegion.NORTHPLAINS.contains(x, y))// || x>200 && x<250 && y>=5 && y<=160)
+    {
+      nPlainLabel.setVisible(true);
+    }
+    else if (ImageRegion.SOUTHPLAINS.contains(x, y))
+    {
+      sPlainLabel.setVisible(true);
+    }
+    else if (ImageRegion.HEARTLAND.contains(x, y))
+    {
+      heartLabel.setVisible(true);
+    }
+    else if (ImageRegion.NORTHEAST.contains(x, y))
+    {
+      neLabel.setVisible(true);
+    }
+    else if (ImageRegion.SOUTHEAST.contains(x, y))
+    {
+      seLabel.setVisible(true);
+    }
+    // }
   }
 
-
+  /**
+   * Method specifically for the smaller map inside of main1.fxml.
+   *
+   * @param event Mouse clicked event
+   */
   @FXML
   public void clickedRegion(MouseEvent event)
   {
-
     double x = event.getX();
     double y = event.getY();
 
-    if(!clickedRegion)
+    makeAllInvisible();
+
+    if (ImageRegion.CALIFORNIA.contains(x, y))
     {
-      if (ImageRegion.CALIFORNIA.contains(x, y))
-      {
-        cali.setVisible(true);
-        clickedRegion = true;
-        System.out.println("Selected cali");
-      }
-      else if (ImageRegion.HEARTLAND.contains(x, y))
-      {
-        heartland.setVisible(true);
-        clickedRegion = true;
-        System.out.println("Selected heartland");
-      }
-      else if (ImageRegion.MOUNTAINST.contains(x, y))
-      {
-        mountSt.setVisible(true);
-        clickedRegion = true;
-        System.out.println("Selected Mountain States");
-      }
-      else if (ImageRegion.NORTHPLAINS.contains(x, y))
-      {
-        nPlains.setVisible(true);
-        clickedRegion = true;
-        System.out.println("Selected North Plains");
-      }
-      else if (ImageRegion.NORTHEAST.contains(x, y))
-      {
-        northSt.setVisible(true);
-        clickedRegion = true;
-        System.out.println("Selected Northeast");
-      }
-      else if (ImageRegion.SOUTHEAST.contains(x, y))
-      {
-        southEast.setVisible(true);
-        clickedRegion = true;
-        System.out.println("Selected Southeast");
-
-      }
-      else if (ImageRegion.SOUTHPLAINS.contains(x, y))
-      {
-        sPlains.setVisible(true);
-        clickedRegion = true;
-        System.out.println("Selected South Plains");
-
-      }
+      cali.setVisible(true);
+      caliLabel.setVisible(true);
+      System.out.println("Selected cali");
     }
+    else if (ImageRegion.HEARTLAND.contains(x, y))
+    {
+      heartland.setVisible(true);
+      heartLabel.setVisible(true);
+      System.out.println("Selected heartland");
+    }
+    else if (ImageRegion.MOUNTAINST.contains(x, y))
+    {
+      mountSt.setVisible(true);
+      mountLabel.setVisible(true);
+      System.out.println("Selected Mountain States");
+    }
+    else if (ImageRegion.NORTHPLAINS.contains(x, y))
+    {
+      nPlains.setVisible(true);
+      nPlainLabel.setVisible(true);
+      System.out.println("Selected North Plains");
+    }
+    else if (ImageRegion.NORTHEAST.contains(x, y))
+    {
+      northSt.setVisible(true);
+      neLabel.setVisible(true);
+      System.out.println("Selected Northeast");
+    }
+    else if (ImageRegion.SOUTHEAST.contains(x, y))
+    {
+      southEast.setVisible(true);
+      seLabel.setVisible(true);
+      System.out.println("Selected Southeast");
+
+    }
+    else if (ImageRegion.SOUTHPLAINS.contains(x, y))
+    {
+      sPlains.setVisible(true);
+      sPlainLabel.setVisible(true);
+      System.out.println("Selected South Plains");
+    }
+
   }
 
+  /**
+   * Method specifically for the larger map where the user chooses their region.
+   *
+   * @param event Mouse clicked event.
+   */
   @FXML
   public void selectInitialRegion(MouseEvent event)
   {
     double x = event.getX();
     double y = event.getY();
 
-    if(!initialRegionSelected)
-    {
-      if (ImageRegion.CALIFORNIA1.contains(x, y))
-      {
-        cali.setVisible(true);
-        initialRegionSelected = true;
-        regionSelected[0] = true;
-        System.out.println("Selected cali");
-      }
-      else if (ImageRegion.HEARTLAND1.contains(x, y))
-      {
-        heartland.setVisible(true);
-        initialRegionSelected = true;
-        System.out.println("Selected heartland");
-      }
-      else if (ImageRegion.MOUNTAINST1.contains(x, y))
-      {
-        mountSt.setVisible(true);
-        initialRegionSelected = true;
-        System.out.println("Selected Mountain States");
-      }
-      else if (ImageRegion.NORTHPLAINS1.contains(x, y))
-      {
-        nPlains.setVisible(true);
-        initialRegionSelected = true;
-        System.out.println("Selected North Plains");
-      }
-      else if (ImageRegion.NORTHEAST1.contains(x, y))
-      {
-        northSt.setVisible(true);
-        initialRegionSelected = true;
-        System.out.println("Selected Northeast");
-      }
-      else if (ImageRegion.SOUTHEAST1.contains(x, y))
-      {
-        southEast.setVisible(true);
-        initialRegionSelected = true;
-        System.out.println("Selected Southeast");
+    makeAllInvisible();
 
-      }
-      else if (ImageRegion.SOUTHPLAINS1.contains(x, y))
-      {
-        sPlains.setVisible(true);
-        initialRegionSelected = true;
-        System.out.println("Selected South Plains");
-      }
+    if (ImageRegion.CALIFORNIA1.contains(x, y))
+    {
+      cali.setVisible(true);
+      initialRegionSelected = true;
+      System.out.println("Selected cali");
     }
-    else
+    else if (ImageRegion.HEARTLAND1.contains(x, y))
     {
+      heartland.setVisible(true);
+      initialRegionSelected = true;
+      System.out.println("Selected heartland");
+    }
+    else if (ImageRegion.MOUNTAINST1.contains(x, y))
+    {
+      mountSt.setVisible(true);
+      initialRegionSelected = true;
+      System.out.println("Selected Mountain States");
+    }
+    else if (ImageRegion.NORTHPLAINS1.contains(x, y))
+    {
+      nPlains.setVisible(true);
+      initialRegionSelected = true;
+      System.out.println("Selected North Plains");
+    }
+    else if (ImageRegion.NORTHEAST1.contains(x, y))
+    {
+      northSt.setVisible(true);
+      initialRegionSelected = true;
+      System.out.println("Selected Northeast");
+    }
+    else if (ImageRegion.SOUTHEAST1.contains(x, y))
+    {
+      southEast.setVisible(true);
+      initialRegionSelected = true;
+      System.out.println("Selected Southeast");
 
+    }
+    else if (ImageRegion.SOUTHPLAINS1.contains(x, y))
+    {
+      sPlains.setVisible(true);
+      initialRegionSelected = true;
+      System.out.println("Selected South Plains");
     }
   }
+
+  private void makeAllInvisible()
+  {
+    cali.setVisible(false);
+    mountSt.setVisible(false);
+    nPlains.setVisible(false);
+    sPlains.setVisible(false);
+    heartland.setVisible(false);
+    southEast.setVisible(false);
+    northSt.setVisible(false);
+  }
+
+  private void hideLabels()
+  {
+    caliLabel.setVisible(false);
+    mountLabel.setVisible(false);
+    nPlainLabel.setVisible(false);
+    sPlainLabel.setVisible(false);
+    heartLabel.setVisible(false);
+    neLabel.setVisible(false);
+    seLabel.setVisible(false);
+  }
+
 
   @FXML
   public void findPolygon(MouseEvent event)
@@ -270,7 +279,7 @@ public class GuiController
     double x = event.getX();
     double y = event.getY();
 
-    System.out.println(x+","+y+",");
+    System.out.println(x + "," + y + ",");
   }
 }
 
