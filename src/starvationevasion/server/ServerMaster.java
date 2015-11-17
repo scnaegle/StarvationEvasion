fa4f8bd39a0e7b292c13958794281357b8d2ccb5
Sean Naegle <sean@naegles.com>
2015-11-17 11:03:59 -0700

    basic placeholder stuff for server master and worker

---
+++ src/starvationevasion/server/ServerMaster.java
@@ -0,0 +1,108 @@
+package starvationevasion.server;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.LinkedList;
+
+/**
+ * Created by Sean Naegle
+ */
+public class ServerMaster
+{
+  private ServerSocket serverSocket;
+  private LinkedList<ServerWorker> allConnections = new LinkedList<ServerWorker>();
+
+  public ServerMaster(int portNumber)
+  {
+    try
+    {
+      serverSocket = new ServerSocket(portNumber);
+    }
+    catch (IOException e)
+    {
+      System.err.println("Server error: Opening socket failed.");
+      e.printStackTrace();
+      System.exit(-1);
+    }
+
+    waitForConnection(portNumber);
+  }
+
+  public void waitForConnection(int port)
+  {
+    String host = "";
+    try
+    {
+      host = InetAddress.getLocalHost().getHostName();
+    }
+    catch (UnknownHostException e)
+    {
+      e.printStackTrace();
+    }
+    while (true)
+    {
+      System.out.println("ServerMaster("+host+"): waiting for Connection on port: "+port);
+      try
+      {
+        Socket client = serverSocket.accept();
+        ServerWorker worker = new ServerWorker(client, this);
+        worker.start();
+        System.out.println("ServerMaster: *********** new Connection");
+        allConnections.add(worker);
+        worker.send("ServerMaster says hello!");
+        worker.send(worker.storeInfoMessage());
+      }
+      catch (IOException e)
+      {
+        System.err.println("Server error: Failed to connect to client.");
+        e.printStackTrace();
+      }
+
+    }
+  }
+
+  public void cleanConnectionList()
+  {
+    for (ServerWorker worker : allConnections) {
+      if (!worker.isAlive()) {
+        allConnections.remove(worker);
+      }
+    }
+  }
+
+  public void broadcast(String s)
+  {
+    for (ServerWorker worker : allConnections)
+    {
+      worker.send(s);
+    }
+  }
+
+  public void closeConnection(ServerWorker worker) {
+    allConnections.remove(worker);
+    cleanConnectionList();
+  }
+  
+  public static void main(String args[])
+  {
+    //Valid port numbers are Port numbers are 1024 through 65535.
+    //  ports under 1024 are reserved for system services http, ftp, etc.
+    int port = 5555; //default
+    if (args.length > 0)
+    try
+    {
+      port = Integer.parseInt(args[0]);
+      if (port < 1) throw new Exception();
+    }
+    catch (Exception e)
+    {
+      System.out.println("Usage: ServerMaster portNumber");
+      System.exit(0);
+    }
+    
+    new ServerMaster(port);
+  }
+}
