c14bf70f8915280bd39482690112e43c33b3eb3b
Tyler Shelton <tshelt0n@unm.edu>
2015-11-19 00:40:39 -0700

    Added line graph code. Excited! I'll get it working completly tomorrow.

--- src/starvationevasion/teamrocket/models/LineGraph.java
+++ src/starvationevasion/teamrocket/models/LineGraph.java
@@ -1,8 +1,18 @@
 package starvationevasion.teamrocket.models;
 
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
 
 import javax.swing.*;
+import javax.swing.text.DefaultFormatter;
 import java.awt.*;
 
 /**
@@ -10,9 +20,32 @@ import java.awt.*;
  */
 public class LineGraph extends ApplicationFrame
 {
-  public LineGraph(String title)
+  public LineGraph(String title, String graphTitle)
   {
     super(title);
+    JFreeChart graph = ChartFactory.createLineChart(graphTitle, "Years",
+        "Crop Output", createDataset(), PlotOrientation.VERTICAL, true, true,
+        false);
+    ChartPanel graphPanel = new ChartPanel(graph);
+    //graph.setPreferredSize(new java.awt.Dimension(560, 367));
+    setContentPane(graphPanel);
+  }
+
+  private DefaultCategoryDataset createDataset()
+  {
+    DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+    dataset.addValue(new Double(50), "Crop Output", "1980");
+    dataset.addValue(new Double(30), "Crop Output", "1981");
+    return dataset;
+  }
+
+  public static void main(String[] args)
+  {
+    LineGraph chart =
+        new LineGraph("Crop Output vs Year", "Amount of Crop Output vs years");
+      chart.pack();
+      RefineryUtilities.centerFrameOnScreen(chart);
+      chart.setVisible(true);
   }
 
 }
