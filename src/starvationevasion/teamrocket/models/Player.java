730af7dcd7a3e79766c41e9e4b8206a75f5f07b3
James Holland <jamesfholland@gmail.com>
2015-11-19 17:17:46 -0700

    Refactored Player's region enum

--- src/starvationevasion/teamrocket/models/Player.java
+++ src/starvationevasion/teamrocket/models/Player.java
@@ -1,10 +1,6 @@
 package starvationevasion.teamrocket.models;
 
-import starvationevasion.common.EnumFood;
-import starvationevasion.common.EnumPolicy;
 import starvationevasion.common.EnumRegion;
-
-import java.net.PortUnreachableException;
 
 /**
  * Created by scnaegl on 11/14/15.
@@ -12,10 +8,10 @@ import java.net.PortUnreachableException;
 public class Player
 {
 
-  private EnumRegion enumRegion;
-
-  private Region region;
-//  private Deck deck;
+  /**
+   * Player's selected region.
+   */
+  public final EnumRegion ENUM_REGION;
 
   /**
    * Creates a new player based on selected region.
@@ -24,22 +20,6 @@ public class Player
    */
   public Player(EnumRegion enumRegion)
   {
-    this.region = new Region(enumRegion);
-//    deck = new Deck(this);
-  }
-
-  public Region getRegion()
-  {
-    return region;
-  }
-
-
-  /**
-   * returns the region that the player is in
-   * @return returns the region that the player is in
-   */
-  public EnumRegion getPlayerRegionEnum()
-  {
-    return region.ENUM_REGION;
+    this.ENUM_REGION = enumRegion;
   }
 }
