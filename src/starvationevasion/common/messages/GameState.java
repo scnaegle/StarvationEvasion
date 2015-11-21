3946cf29668b8bfb0d532d553fcb725371823059
Sean Naegle <sean@naegles.com>
2015-11-21 13:20:47 -0700

    added a waiting for connections game state

--- src/starvationevasion/common/messages/GameState.java
+++ src/starvationevasion/common/messages/GameState.java
@@ -4,7 +4,8 @@ package starvationevasion.common.messages;
  * Created by scnaegl on 11/20/15.
  */
 public enum GameState {
-  CARD_PLAYING,
+  WAITING_FOR_CONNECTIONS,
+  TURN,
   VOTING,
   SIMULATION,
   VISUALIZATION,
