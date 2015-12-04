a0b71ef9ace800141f7caeae82ec6d136f711c4b
James Holland <jamesfholland@gmail.com>
2015-12-04 01:11:41 -0700

    Added place holder for chat updates

--- src/starvationevasion/teamrocket/gui/ChatController.java
+++ src/starvationevasion/teamrocket/gui/ChatController.java
@@ -1,11 +1,16 @@
 package starvationevasion.teamrocket.gui;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.util.Duration;
 import starvationevasion.teamrocket.main.Main;
 
 import java.net.URL;
@@ -27,6 +32,26 @@ public class ChatController implements javafx.fxml.Initializable
   private CharSequence text;
   private boolean hasText = false;
   /**************************/
+
+  public ChatController()
+  {
+    Timeline updater = new Timeline(new KeyFrame(Duration.millis(Main.GUI_REFRESH_RATE), new EventHandler<ActionEvent>()
+    {
+      @Override
+      public void handle(ActionEvent event)
+      {
+        //Get new messages.
+
+        if(Main.GAME_CLOCK.getTimeLeft() <=0)
+        {
+          //Main.getGameController().finishedCardDraft();
+        }
+      }
+    }));
+
+    updater.setCycleCount(Timeline.INDEFINITE);
+    updater.play();
+  }
 
   @Override
   public void initialize(URL location, ResourceBundle resources)
