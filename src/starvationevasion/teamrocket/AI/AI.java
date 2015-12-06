4996858484b7000069d5dd6ec2ed1136aead54a9
Sean Naegle <sean@naegles.com>
2015-12-05 18:57:36 -0700

    added fixed for casting error everywhere

--- src/starvationevasion/teamrocket/AI/AI.java
+++ src/starvationevasion/teamrocket/AI/AI.java
@@ -103,7 +103,7 @@ public class AI extends Player
       PolicyCard[] cards = AI.selectCards(getHandCards(),generator, actionsPerformed);
       setCardTargets(cards);
       actionsPerformed = 2;
-      return (EnumPolicy[]) Stream.of(cards).map(c -> c.getCardType()).toArray();
+      return Stream.of(cards).map(c -> c.getCardType()).toArray(size -> new EnumPolicy[size]);
     }
     return null;
   }
