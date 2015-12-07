60c94e95621f41ea9329cfd203cd64ee5a4bedfb
Sean Naegle <sean@naegles.com>
2015-12-07 15:58:10 -0700

    added methods to get the region history as either a hashmap or an array

--- src/starvationevasion/teamrocket/models/Player.java
+++ src/starvationevasion/teamrocket/models/Player.java
@@ -278,6 +278,26 @@ public class Player implements PlayerInterface
     }
   }
 
+  /**
+   * Returns a Map of all the region histories.
+   * @return
+   */
+  public Map<EnumRegion, RegionHistory> getRegionHistories() {
+    return regionHistories;
+  }
+
+  /**
+   * Returns an array of the region histories as apposed to the default
+   * hashmap being stored.
+   * @return
+   */
+  public RegionHistory[] getRegionHistoriesArray() {
+    RegionHistory[] regionHistoriesArray = new RegionHistory[EnumRegion.SIZE];
+    for(Map.Entry<EnumRegion, RegionHistory> entry : regionHistories.entrySet()) {
+      regionHistoriesArray[entry.getKey().ordinal()] = entry.getValue();
+    }
+    return regionHistoriesArray;
+  }
 
   synchronized public void setGameState(EnumGameState gameState) {
     this.gameState = gameState;
