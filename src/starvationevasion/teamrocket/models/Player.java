1b1a8151a1cd6d4527ae0650e102074500127d33
Sean Naegle <sean@naegles.com>
2015-12-07 20:33:44 -0700

    return null if there is no hand yet

--- src/starvationevasion/teamrocket/models/Player.java
+++ src/starvationevasion/teamrocket/models/Player.java
@@ -125,7 +125,11 @@ public class Player implements PlayerInterface
 
   @Override
   public PolicyCard getCard(int card_index) {
-    return PolicyCard.create(ENUM_REGION, hand[card_index]);
+    try {
+      return PolicyCard.create(ENUM_REGION, hand[card_index]);
+    } catch (NullPointerException e) {
+      return null;
+    }
   }
 
   /**
